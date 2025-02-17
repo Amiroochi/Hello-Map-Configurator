2f60a17f7d15e7424a50028a13d4e0783a617694
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2025-02-17 22:53:38 +0330

    Enable mocking on production too

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import App from "./app";
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== "development") return;
+  // if (process.env.NODE_ENV !== "development") return;
 
   const { worker } = await import("./__mocks__/browser");
   return worker.start();
