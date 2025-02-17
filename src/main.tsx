2422e48f2bc13db472d920b5e120dc2e283a3c85
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2025-02-17 15:37:39 +0330

    Update mocks folder ref

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from "./app";
 async function enableMocking() {
   if (process.env.NODE_ENV !== "development") return;
 
-  const { worker } = await import("./mocks/browser");
+  const { worker } = await import("./__mocks__/browser");
   return worker.start();
 }
 
